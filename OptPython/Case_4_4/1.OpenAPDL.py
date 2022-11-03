66af7e5cf9c722899d1d3b4414cb618ec575cba6
SoledadChubretovic <schubretovic1@uc.cl>
2022-11-03 13:04:14 -0300

    crear modulo para abrir

---
+++ OptPython/Case_4_4/1.OpenAPDL.py
@@ -0,0 +1,12 @@
+
+#%% Open APDL license
+
+from ansys.mapdl.core import Mapdl, launch_mapdl
+mapdl = launch_mapdl()
+Mapdl(start_instance=True)
+print(mapdl)
+
+# %%
+hola = [(1:4)]
+print(hola)
+# %%
