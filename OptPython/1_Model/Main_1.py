676af792f99b231efabf24c9089744c9a22471fb
SoledadChubretovic <schubretovic1@uc.cl>
2022-11-17 08:13:54 -0300

    menos gen para probar tabla

--- OptPython/1_Model/Main_1.py
+++ OptPython/1_Model/Main_1.py
@@ -221,15 +221,15 @@ for i in range(0,len(data1)):
     linedata[-2] = linedata[-2]*2
     table_data.append(linedata)
 
-# column tags
-column_names = ["Weight [kg]","U_value [W/m2K]"]
-for i in range(0,N_VARIABLES - 3):
-    column_names = column_names.append(["l" + str(i + 1) + " [mm]"])
-column_names = column_names.append(["w [mm]"])
-column_names = column_names.append(["W [mm]"])
-column_names = column_names.append(["λ_clay [W/mK]"])
-
-result_table = tabulate(table_data, column_names, tablefmt = "plain")
+# # column tags
+# column_names = ["Weight [kg]","U_value [W/m2K]"]
+# for i in range(0,N_VARIABLES - 3):
+#     column_names = column_names.append(["l" + str(i + 1) + " [mm]"])
+# column_names = column_names.append(["w [mm]"])
+# column_names = column_names.append(["W [mm]"])
+# column_names = column_names.append(["λ_clay [W/mK]"])
+#%%
+result_table = tabulate(table_data, tablefmt = "plain")
 
 # write table to results file
 results = open(r"C:\Users\nchubretovic\OneDrive - Entel\Escritorio\Sole\Tesis\OptPython\1_Model\OptPython_Log.txt", "w")
